13606e2ba16c4b1afb387a3e88b8b24078cf47c6
josenicoleno <josenicoleno@gmail.com>
2022-06-22 22:29:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-mj2vgk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-mj2vgk)
---
+++ index.tsx
@@ -0,0 +1,6 @@
+let pets =  ["cat", "dog", "rabbits"]
+let carnivorous = ["lion", "wolf", "leopard"]
+
+let animals = [...pets,...carnivorous]
+console.log(pets.concat(carnivorous))
+console.log(animals)
